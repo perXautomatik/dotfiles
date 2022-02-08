c5634d7d546871121956b181418ffc1af2217ded
perXautomatik <christoffer.broback@gmail.com>
2022-02-08 18:33:04 +0100

    asd
    Former-commit-id: 92209f907a0f81f67faada238f688731f3821fdb

--- AppData/Roaming/JetBrains/.DataGrip2021.1/consoles/db/46f0d266-a303-46c1-8f11-eebf52313160/console_1.sql
+++
@@ -1,5 +0,0 @@
-
-update tasks
-set subject = rightstr(subject,length(subject)-2)
-WHERE created_datetime > datetime()
-and instr(subject,"- ") = 1
--- AppData/Roaming/Vortex/plugins/Vortex Extension Update - Valheim Support v1.0.14/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IGithubRepo {
-  name: string;
-  url: string;
-  website: string;
-  filePattern: RegExp;
-  coerceVersion: (version: string) => string;
-  latest?: string;
-  current?: string;
-}
