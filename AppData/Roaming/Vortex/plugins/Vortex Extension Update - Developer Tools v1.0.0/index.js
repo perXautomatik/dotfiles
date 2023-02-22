6b6da38107df425d0aa8137485996ae26e733a8b
perXautomatik <christoffer.broback@gmail.com>
2023-02-22 17:46:55 +0100

    etc
    (cherry picked from commit f321398890e7be89b588823eb022c78395fc7aae)
    
    # Conflicts:
    #	Local/Copy Handler/ch.log
    
    etc
    
    (cherry picked from commit 1e593010ad872194c0cea351c45872683f5113d3)
    (cherry picked from commit d45d8217b0e4ab49b76d4ce51965bec853aa2bb0)

--- AppData/Roaming/Vortex/plugins/Vortex Extension Update - Developer Tools v1.0.0/index.js
+++
@@ -1,9 +0,0 @@
-function init(context) {
-  context.registerAction('global-icons', 300, 'settings', {}, 'Developer Tools', () => {
-    require('electron').remote.getCurrentWebContents().toggleDevTools();
-  });
-
-  return true;
-}
-
-exports.default = init;
